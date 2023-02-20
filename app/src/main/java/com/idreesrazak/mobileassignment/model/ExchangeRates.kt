212b5c447e4cbb16a9a797962642a81cc087d215
Ashiq Muhammad <muhammadashiqse10@gmail.com>
2023-02-20 16:11:51 +0100

    Files changed

--- app/src/main/java/com/idreesrazak/mobileassignment/model/ExchangeRates.kt
+++
@@ -1,8 +0,0 @@
-package com.idreesrazak.mobileassignment.model
-
-data class ExchangeRates(
-    val amount: Int,
-    val base: String?,
-    val date: String?,
-    val rates: Rates?
-)
--- app/src/main/java/com/idreesrazak/mobileassignment/util/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.idreesrazak.mobileassignment.util
-
-class Constants {
-
-    companion object{
-        const val BASE_URL = "api.frankfurter.app"
-    }
-}
